5f584d1fd9c79f65e34d721685e2f809a4f442fe
XxByNorthxX <37373359+XxByNorthxX@users.noreply.github.com>
2018-04-18 09:13:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -40,4 +40,87 @@ int main()
     return 0;
 }
 
+22222222222222222
+#include <iostream>
+
+using namespace std;
+
+int esPrimo(int x){
+    for (int i=2; i < x; i++ )
+      if ( x  % i == 0 ) return 0;
+
+    return 1;}
+
+int printNumber(int n){
+    for(int i=1;i<n;i++)
+        if (esPrimo(i)==1)
+            cout<<i<<" ";
+
+}
+
+int main(){
+    printNumber(35);
+
+
+return 0;
+}
+3333333333333333333333333333
+#include <iostream>
+
+using namespace std;
+
+int potencia(int x, int y){
+    if(y==1)
+        return x;
+    else
+        return x*potencia( x, y-1);
+
+
+}
+
+
+int main(){
+    cout<<potencia(2,4);
+
+
+return 0;
+}
+
+44444444444444444444444444444444
+    
+#include <iostream>
+
+using namespace std;
+
+void changeVar(int &a,int &b){
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+}
+void inter(int a, int b){
+    a^=b;
+    b^=a;
+    a^=b;
+    cout<<a<<endl;
+    cout<<b<<endl;
+}
+
+int main(){
+    int a=1,b=2;
+    int x=8,y=9;
+    changeVar(a,b);
+    
+    cout<<a<<" "<<b<<endl;
+
+    inter(x,y);
+
+
+
+
+
+
+return 0;
+}
+
 
